879220d50bf24f4f9e7f70271d44e28b93170a44
SalvaChLl <salvachll@gmail.com>
2021-11-10 20:54:42 +0100

    Correccion dependencias

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+const fs = require('fs')
 import csv from 'csvtojson'
 
 export async function convertCSVToJSON(data: string) {
+  console.log(data)
   console.log('PARSING CSV')
   try {
     const res = await csv({
@@ -11,3 +13,10 @@ export async function convertCSVToJSON(data: string) {
     throw Error(error)
   }
 }
+
+async function parseData() {
+  fs.writeFileSync('./VAL.json', JSON.stringify(await convertCSVToJSON(fs.readFileSync('./directorio-de-bibliotecas-valencianas_2020.csv').toString()).toString()));
+}
+
+// parseData()
+// fs.writeFileSync('./VAL.json', JSON.stringify({ bibliotecas: fs.readFileSync('./directorio-de-bibliotecas-valencianas_2020.csv').toString() }));
